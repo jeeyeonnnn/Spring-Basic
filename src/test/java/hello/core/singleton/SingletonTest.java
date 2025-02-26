4004c95e37a32caff774ec65489c221a22009363
jeeyeonnnn <jeeyeonnn@naver.com>
2025-02-26 14:28:00 +0900

    test: singleton test case

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,54 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        // 1. 조회 : 호출할 때마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // 검증
+        assertThat(memberService1).isNotSameAs(memberService2);
+
+        /*
+
+        스프링 없는 순수한 DI 컨테이너인 AppConfig는 요청할 때마다 객체를 새로 생성한다.
+        즉, 메모리 낭비가 심하다.
+
+        해결방안은 해당 객체가 딱 1개만 생성되고, 공유하도록 설계!!!  => 싱글톤 패턴
+
+        ** 싱글톤 패턴
+        - 클래스의 인스턴스가 딱 1개만 생성되는 것을 보장하는 디자인 패턴이다.
+        - private 생성자를 이용해서 외부에서 임의로 "new" 키워드를 사용하지 못하도록 막아야 한다.
+
+         */
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        System.out.println("instance1 = " + instance1);
+        System.out.println("instance2 = " + instance2);
+
+        assertThat(instance1).isSameAs(instance2);
+    }
+}
