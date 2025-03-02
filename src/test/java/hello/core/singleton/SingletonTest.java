8bc2ab1dce56748092ff4dcf3ef62a95cfcd0a6b
jeeyeonnnn <jeeyeonnn@naver.com>
2025-03-02 18:48:44 +0900

    test: spring singleton error test

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,9 +2,9 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -50,5 +50,27 @@ public class SingletonTest {
         System.out.println("instance2 = " + instance2);
 
         assertThat(instance1).isSameAs(instance2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        AnnotationConfigApplicationContext appConfig = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회 : 호출할 때마다 객체를 생성
+        MemberService memberService1 = appConfig.getBean("memberService", MemberService.class);
+        MemberService memberService2 = appConfig.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // 검증
+        assertThat(memberService1).isSameAs(memberService2);
+
+        /*
+        스프링의 기본 빈 등록 방식은 싱글톤이지만, 싱글톤 방식만 지원하는 것은 아님!
+        요청할 때마다 새로운 객체를 생성해서 반환하는 기능도 제공함 => 하지만 사용 확률 약 1% 정도 (?)
+         */
     }
 }
