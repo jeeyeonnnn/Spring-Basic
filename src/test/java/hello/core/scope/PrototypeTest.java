6285942e62c1bc2ef7b202d405acd1bec4db2407
jeeyeonnnn <jeeyeonnn@naver.com>
2025-03-06 10:32:31 +0900

    test: prototype scope test

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,37 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+
+        Assertions.assertThat(bean1).isNotSameAs(bean2);
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destory(){
+            System.out.println("SingletonBean.destory");
+        }
+    }
+}
